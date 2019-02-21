680a2455e214082724d1031cbc6fe5988a840e3c
Dael <dael.separa@gmail.com>
2019-02-21 10:21:35 +0800

    Initial commit

---
+++ DeepNeuralNetworkClassifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using Gtk;
+
+namespace DeepNeuralNetworkClassifier
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+        }
+    }
+}
